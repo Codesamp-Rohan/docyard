5d4a11da3aa25c9b7db67220752d93ce98ae732b
Rohan Chaudhary <121369255+Codesamp-Rohan@users.noreply.github.com>
2023-03-11 20:43:51 +0530

    9876543

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-function showoption(){
-    const a = document.getElementById("subbtn").style.visibility = "visible";
-}
-function hideoption(){
-    const b = document.getElementById("subbtn").style.visibility = "hidden";
-}
-
-// function getCurrentDate() {
-//     let date = new Date().toDateString();
-//     document.querySelector('.output').textContent = date;
+// function showoption(){
+//     const a = document.getElementById("subbtn").style.visibility = "visible";
 // }
+// function hideoption(){
+//     const b = document.getElementById("subbtn").style.visibility = "hidden";
+// }
+function myFunction() {
+    document.getElementById("subbtn").classList.toggle("show");
+  }
+
+window.onclick = function(event) {
+    if (!event.target.matches('.mainbutton')) {
+      var dropdowns = document.getElementById("subbtn");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
